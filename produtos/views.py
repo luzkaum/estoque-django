f14e633a1c25bc430a019ed3c2a8c92aed6d6690
luzkaum <yzedy14@gmail.com>
2026-07-23 14:28:12 -0300

    New: Pagina cadastro de produto

--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
+from .forms import ProdutoForm
 
 def listar_produtos(request):
     produtos = Produto.objects.all()
     return render(request, 'produtos/lista.html', {'produtos': produtos})
+
+def criar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'produtos/form.html', {'form': form})
