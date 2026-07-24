814b536088fa77b0ca029d28c0fece07e739cfca
luzkaum <yzedy14@gmail.com>
2026-07-24 14:40:42 -0300

    New: Adicioneiadicionei categorias (relacionamento ForeignKey) e coluna na lista

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Categoria
 
 admin.site.register(Produto)
+admin.site.register(Categoria)
--- produtos/models.py
+++ produtos/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+
+class Categoria(models.Model):
+    nome = models.CharField('Nome', max_length=80)
+
+    def __str__(self):
+        return self.nome
+
 class Produto(models.Model):
     nome = models.CharField('Nome do produto', max_length=120)
     descricao = models.TextField('Descrição', blank=True)
     preco = models.DecimalField('Preço', max_digits = 10, decimal_places=2)
     quantidade = models.PositiveIntegerField('Quantidade em estoque', default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank= True, verbose_name='Categoria')
     criado_em = models.DateTimeField('Criado em', auto_now_add=True)
 
     def __str__(self):
