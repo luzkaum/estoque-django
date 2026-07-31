0415bfba29f09a1bff63c6c4ec9a92c90e505a95
luzkaum <yzedy14@gmail.com>
2026-07-31 14:24:53 -0300

    Fix: remoção de chaves expostas e implementação de variaveis de ambiente

--- config/settings.py
+++ config/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR/'.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6rqozkb-$t7*v&p5%b09!5^p@b3aq%ja!j#s771e5iz_8za@*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
--- produtos/urls.py
+++ produtos/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='produtos/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('relatorio/', views.relatorio, name='relatorio'),
 ]
